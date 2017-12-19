eb708b075845793e3fdcbdc90b0a924ddc56835f
Felipe Gimenez <lipe-gimenez@hotmail.com>
2017-12-19 16:11:43 -0200

    Add fields to OrgComapnies

--- db/migrate/20171219151843_create_org_companies.rb
+++ db/migrate/20171219151843_create_org_companies.rb
@@ -1,7 +1,12 @@
 class CreateOrgCompanies < ActiveRecord::Migration[5.1]
   def change
     create_table :org_companies do |t|
-
+      t.string :name, null: false
+      t.string :avatar
+      t.text :description
+      t.string :verification_doc
+      t.boolean :verified
+      t.belongs_to :typ_company
       t.timestamps
     end
   end
