21f8b4b6378059dc82a1f33b4b5ef67e69845394
Felipe Gimenez <lipe-gimenez@hotmail.com>
2017-12-19 16:10:41 -0200

    Add properties to OrgCompany

--- app/models/org_company.rb
+++ app/models/org_company.rb
@@ -1,2 +1,6 @@
 class OrgCompany < ApplicationRecord
+    has_and_belong_to_many :org_contacts
+    has_many :org_persons
+    has_many :orgs_products
+    belongs_to :typ_company, foreign_key: "type_company_id"
 end
